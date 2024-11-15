c2df70fa5d7f2c258a7f687757b73625828563af
Chris Apps <chrisapps@Chriss-MacBook-Air.local>
2024-11-15 14:04:44 +1000

    more files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Import packages
+import os
 from flask import Flask
 # Import from init.py
 from init import db, ma
@@ -7,7 +8,7 @@ def create_app():
     # Initialise
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://lms_dev:123456@localhost:5432/lms_app_db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
 
     db.init_app(app)
     ma.init_app(app)
