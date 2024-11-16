79ec8bed3b847da8970707c9d1796969df75295e
Chris Apps <chrisapps@Chriss-MacBook-Air.local>
2024-11-16 10:39:33 +1000

    Update and delete routes added to student controller

--- controllers/student_controller.py
+++ controllers/student_controller.py
@@ -28,33 +28,47 @@ def get_student(student_id):
 # Create - /students - POST
 @students_bp.route("/", methods=["POST"])
 def create_student():
+      # Get information from request body
       body_data = request.get_json()
+      # Create student instance
       new_student = Student(
            name = body_data.get("name"),
            email = body_data.get("email"),
            address = body_data.get("address")
       )
+      # Add new student data
       db.session.add(new_student)
+      # Commit changes
       db.session.commit()
       return StudentSchema().dump(new_student), 201
-      
-# @app.route("/products", methods=["POST"])
-# def create_product():
-#     # Import request from flask
-#     body_data = request.get_json()
-#     # New instance of Product class and use keys from body_data to assign values
-#     new_product = Product(
-#         name = body_data.get("name"),
-#         description = body_data.get("description"),
-#         price = body_data.get("price"),
-#         stock = body_data.get("stock")
-#     )
-#     db.session.add(new_product)
-#     db.session.commit()
-#     return product_schema.dump(new_product), 201 # 201 Created code returned
 
 # Update - /students/id - PUT or PATCH
+@students_bp.route("/<int:student_id>", methods=["PUT", "PATCH"])
+def update_student(student_id):
+     stmt = db.select(Student).filter_by(id=student_id)
+     student = db.session.scalar(stmt)
+     body_data = request.get_json()
 
+     if student:
+          # Update with new data or use existing if new data not provided
+          student.name = body_data.get("name") or student.name
+          student.email = body_data.get("email") or student.email
+          student.address = body_data.get("address") or student.address
+          db.session.commit()
+          return StudentSchema().dump(student)
+     else:
+          # Error message if student doesn't exist
+          return {"message": f"Student with id: {student_id} does not exist"}, 404
 
 # Delete - /students/id - DELETE
+@students_bp.route("/<int:student_id>", methods=["DELETE"])
+def delete_student(student_id):
+     stmt = db.select(Student).filter_by(id=student_id)
+     student = db.session.scalar(stmt)
 
+     if student:
+          db.session.delete(student)
+          db.session.commit()
+          return {"message": f"Student: '{student.name}' deleted successfully"}
+     else:
+          return {"message": f"Student with id: '{student_id}' does not exist"}, 404
