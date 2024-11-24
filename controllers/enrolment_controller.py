0fab9e86d46741d61560487025274bd4735434f7
Chris Apps <chrisapps@Chriss-MacBook-Air.local>
2024-11-24 11:37:16 +1000

    added POST and DELETE routes

--- controllers/enrolment_controller.py
+++ controllers/enrolment_controller.py
@@ -27,10 +27,42 @@ def get_enrolment(enrolment_id):
         return {"message": f"Enrolment with id '{enrolment_id}' does not exist"}, 404
 
 
-# CREATE an enrolment (/enrolments)
+# POST an enrolment (/enrolments)
+@enrolments_bp.route("/", methods=["POST"])
+def create_enrolment():
+    try:
+        body_data = request.get_json()
 
+        new_enrolment = Enrolment(
+            enrolment_date = body_data.get("enrolment_date"),
+            student_id = body_data.get("student_id"),
+            course_id = body_data.get("course")
+        )
+        db.session.add(new_enrolment)
+        db.session.commit()
+        return EnrolmentSchema().dump(new_enrolment), 401
+    except IntegrityError as err:
+        print(err.orig.pgcode)
+        if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+            # not_null_violoation
+            # Return specific field that is in violoation
+            return {"message": f"The field '{err.orig.diag.column_name}' is required"}, 409
+        if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+                # unique_constraint_violoation
+                return {"message": "Data already exists. Update details instead"}, 409
 
-# PUT or PATCH an enrolment (/enrolments/course_id)
+# PUT or PATCH an enrolment (/enrolments/enrolment_id)
 
 
 # DELETE an enrolment (/enrolments)
+@enrolments_bp.route("/<int:enrolment_id>", methods=["DELETE"])
+def delete_enrolment(enrolment_id):
+     stmt = db.select(Enrolment).filter_by(id=enrolment_id)
+     enrolment = db.session.scalar(stmt)
+
+     if enrolment:
+          db.session.delete(enrolment)
+          db.session.commit()
+          return {"message": f"enrolment: '{enrolment.id}' deleted successfully"}
+     else:
+          return {"message": f"Enrolment with id: '{enrolment_id}' does not exist"}, 404
